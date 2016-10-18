7135050d54c85d18db1c7af9abf9efa286b126c1
velechva <velechva@mcmaster.ca>
2016-10-18 23:36:53 +0000

    Fixed library.js

--- src/library.js
+++ src/library.js
@@ -8,24 +8,7 @@ var path = require('path')
 	@return - a list of n songs that will be the new voting options
 */
 function getSongs(p) {
-	array = []
-
-	fs.readdirSync(p, function (err, files) {
-		if (err) { console.log(err) }
-
-	  files.map(function (file) {
-	      return path.join(p, file)
-	  }).filter(function (file) {
-	      return fs.statSync(file).isFile()
-	  }).forEach(function (file) {
-	  	// add to array variable
-	  	array.push(path.basename(file))
-	  	console.log(array[0])
-	  })
-
-	})
-
-	return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']
+	return fs.readdirSync(p)
 }
 
 exports.getSongs = getSongs
