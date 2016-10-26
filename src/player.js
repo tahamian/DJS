e2b66f7ae015d40c73c99699518086baff0391bf
velechva <velechva@mcmaster.ca>
2016-10-26 09:27:50 -0400

    Merge conflict & Example comments added

--- src/player.js
+++ src/player.js
@@ -1,5 +1,17 @@
+/**
+ * @module player
+ * @version 1.0
+*/
+
 var player = require('play-sound')(opts = {})
 
+/**
+ * Plays a song
+ * @param {string} song - Complete path of the song file
+ * @param {function} done - callback function to be called when song is done playing
+ * @author Victor Velechovsky
+ * @version 0.1
+*/
 function play(song, done) {
 	songPath = __dirname + '/music/' + song
 	console.log('Playing song: ' + song)
