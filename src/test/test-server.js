9b6c119f7a0e571248371e4a2bcfd76859fbc157
velechva <velechva@mcmaster.ca>
2016-11-12 16:36:19 -0500

    Trying webdriver-sync

--- src/test/test-server.js
+++ src/test/test-server.js
@@ -7,40 +7,43 @@ var driver = new webdriver.Builder()
     .forBrowser('firefox')
     .build()
 
-var MUSIC = "02 Let Em' Know.mp3\n03 Exchange.mp3\n04 For However Long.mp3\n05 Don't.mp3\n06 Open Interlude.mp3"
+const BUTTON_NAMES = ["03 Exchange.mp3", "04 For However Long.mp3", "05 Don't.mp3", "06 Open Interlude.mp3", "Drake.mp3"]
+const TITLE = "DJS"
 
 describe('Client Side Graphical Interface', () => {
-/*
+
     it('Wepage Title Loaded', (done) => {
+
         driver.get('localhost:3000')
         driver.getTitle().then((title) => {
             expect(title).to.equal('DJS')
             done()
         })
+
     }).timeout(5000)
 
     it('Buttons Loaded', (done) => {
-        var list = driver.findElement(By.id('list-div'))
-        list.getText().then(text => {
-            console.log('list.getText(): ' + list.getText())
-            expect(text).to.equal(MUSIC)
-            done()
-        })
-    })
 
-    it('Buttons Includes Song Title', (done) => {
-        var list = driver.findElement(By.id('list-div'))
-        list.getText().then(text => {
-            expect(text).to.equal(MUSIC)
-            done()
-        })
+        driver.findElements(By.className('list-group-item list-group-item-action'))
+            .then(children => {
+
+                var flag = true
+                
+                for (var i = 0; i < children.length; i++) {
+                    var child = children[i].getText().then(text => {
+                        console.log(BUTTON_NAMES[0])
+                        if (text != BUTTON_NAMES[i]) flag = false
+                    })
+                }
+
+                expect(flag).to.equal(true)
+                done()
+
+            })
     })
 
     it('Vote causes button to be highlighted', (done) => {
-        var list = driver.findElement(By.id('list-div'))
-        var children = list.findElements(By.xpath('.//*'))
-        console.log(children [0])
         done()
     })
-    */
+    
 })
