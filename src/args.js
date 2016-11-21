6624ab08cd755eae588ae84af6a1266ccb579cc8
velechva <velechva@mcmaster.ca>
2016-11-21 10:08:51 -0500

    Fixed merge conflict in args.js

--- src/args.js
+++ src/args.js
@@ -1,8 +1,4 @@
 /**
-<<<<<<< HEAD
-* Options for command line arguments
-*@constant
-=======
  * @module args
  * @version 1.0
  *
@@ -14,7 +10,6 @@
 	* Creates the command line flags
     *
 	* @return A JSON object that contains the flag options
->>>>>>> 92b6b4121191c72209a6c2e3335b598127a9cba9
 */
 const options = [
     // -m flag to select a custom music directory
